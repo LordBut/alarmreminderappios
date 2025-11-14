5d227121941f7c03bc2247752f5b9355b59615c4
Ethan <ethanbut08@gmail.com>
2025-11-14 13:48:57 +0000

    bruuh

--- lib/screens/membership_screen.dart
+++ lib/screens/membership_screen.dart
@@ -68,7 +68,7 @@ class _MembershipScreenState extends State<MembershipScreen> {
                 ),
               ),
             );
-          }).toList()
+          })
         ],
       ),
     );
